f9c6f086ae48872b5096c1f443f4b1f452a97d66
valdivix-coder <valdivix@gmail.com>
2026-09-16 16:52:32 -0300

    Configurar icono e instalación PWA para iPhone y Android

---
+++ service-worker.js
@@ -0,0 +1,37 @@
+const CACHE = '18z-shell-v1';
+const CORE = [
+  '/',
+  '/inicio.html',
+  '/manifest.webmanifest',
+  '/icons/apple-touch-icon.png',
+  '/icons/icon-192.webp',
+  '/icons/icon-512.webp'
+];
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(CORE)).catch(() => {})
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE).then(cache => cache.put(event.request, copy)).catch(() => {});
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
